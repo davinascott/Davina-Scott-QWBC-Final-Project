4e9f84acdde569d360423393b31e8904c8b7f9ef
davinascott <davinascott0401@gmail.com>
2022-12-14 21:27:43 -0500

    Added mocha reports to the project

--- cypress.config.js
+++ cypress.config.js
@@ -9,5 +9,16 @@ module.exports = defineConfig({
     //This secction sets up the base url and the basic spec pattern that will be used
     baseUrl: `https://ui-automation-camp.vercel.app`,
     specPattern: 'cypress/e2e/test/*.cy.{js,jsx,ts,tsx}',
-  },
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+    },
+    
+    chromeWebSecurity: false
+  }
+  
 });
